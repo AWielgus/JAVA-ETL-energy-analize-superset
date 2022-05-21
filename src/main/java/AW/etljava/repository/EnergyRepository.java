c2854b78591562dbd6cb953b91f59db9564b8b92
Adrian Wielgus <61666115+AWielgus@users.noreply.github.com>
2022-05-21 11:20:49 +0200

    Delete src directory

--- src/main/java/AW/etljava/repository/EnergyRepository.java
+++
@@ -1,8 +0,0 @@
-package AW.etljava.repository;
-
-import AW.etljava.model.Energy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnergyRepository extends JpaRepository<Energy, Long> {
-
-}
